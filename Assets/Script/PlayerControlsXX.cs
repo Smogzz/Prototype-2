94eead6e9bc3419ea8ffce4a1f64f560c54e3cce
Smogzz <144461630+Smogzz@users.noreply.github.com>
2024-09-30 12:41:50 -0400

    2.3 update

--- Assets/Script/PlayerControlsXX.cs
+++ Assets/Script/PlayerControlsXX.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerControlsXX : MonoBehaviour
 {
     float horizontalInput;
+    public float xRange = 10;
+    public float speed = 15;
+    public GameObject progectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,19 +18,24 @@ public class PlayerControlsXX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //get the players horizontal input//
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * 5 * Time.deltaTime * horizontalInput);
-
-        if(transform.position.x < -10)
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontalInput);
+        //Keep the player at xrange left//
+        if(transform.position.x < - xRange)
         {
-            transform.position = new Vector3 (-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3 (-xRange, transform.position.y, transform.position.z);
         }
         
-         if(transform.position.x > 10)
+         if(transform.position.x > xRange)
         {
-            transform.position = new Vector3 (10, transform.position.y, transform.position.z);
+            transform.position = new Vector3 (xRange, transform.position.y, transform.position.z);
         }
         
-
+        //check the player is pressing space//
+        if(Input.GetKeyDown(KeyCode.Space))
+        { 
+                //launch projectile from the player//
+        }
     }
 }
